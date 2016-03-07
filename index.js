82a7afdf841947b2c9633f8c2bf1c230d351bd93
bubenshchykov <a.bubenshchykov@gmail.com>
2016-03-07 21:34:04 +0200

    first master

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 var express = require('express');
 var app = express();
 
-// common
-console.log('starting..');
-
-app.use(function(req, res, next) {
-	setTimeout(function() {
-		console.log('handler 1', req.path);
-		next();	
-	}, 0);	
-});
-
-app.use(function(req, res, next) {
-	console.log('handler 2', req.path);
-	next();
-});
-
-// api routes
-
 app.get('/api/*', function (req, res) {
   res.send('i am api');
 });
 
-app.get('/master.html', function (req, res) {
-  res.send('i am master');
+app.get('*', function (req, res) {
+  res.sendFile('./static/index.html', {root: __dirname});
 });
  
 app.listen(8080);
-console.log('anuka');
-
